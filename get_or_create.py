f3ddbea341976cae2165f29a7f2d07a9b34088c4
Daniel Foster <daniel.a.foster@gmail.com>
2020-11-22 11:49:00 +0100

    remove db files outside of directory

--- get_or_create.py
+++
@@ -1,9 +0,0 @@
-def get_or_create(session, Model, defaults=None, **kwargs):
-    instance = session.query(Model).filter_by(**kwargs).first()
-    if instance:
-        return instance
-    else:
-        kwargs |= defaults or {}
-        instance = Model(**kwargs)
-        session.add(instance)
-        return instance
