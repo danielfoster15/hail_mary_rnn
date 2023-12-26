0072ae4553ae6ca2f1ca923b37e29bcc56052c40
Daniel Foster <daniel.a.foster@gmail.com>
2023-12-26 15:39:39 +0100

    get player vectors on fly to not see future

--- hail_mary_rnn/model.py
+++ hail_mary_rnn/model.py
@@ -11,15 +11,20 @@ from keras.callbacks import ModelCheckpoint
 # split a univariate sequence into samples
 
 
-def get_game_sequences_for_teams(games, teams, vector_dict):
+def get_game_sequences_for_teams(games, teams):
     # X is input, a sequence of 3 games
     X, y = list(), list()
     for team in teams:
-        game_vectors = get_vector_by_team_and_game(games, team, vector_dict)
-        for team_vector, opponent_vector, team_score, opponent_score, date in game_vectors:
-            input_X = np.sum(team_vector, axis=0) - \
-                np.sum(opponent_vector, axis=0)
-           # print(input_X.shape)
+        game_vectors = get_vector_by_team_and_game(games, team)
+        for (
+            team_vector,
+            opponent_vector,
+            team_score,
+            opponent_score,
+            date,
+        ) in game_vectors:
+            input_X = np.sum(team_vector, axis=0) - np.sum(opponent_vector, axis=0)
+            # print(input_X.shape)
             if team_score > opponent_score:
                 output_y = np.array([1, 0, 0])
             elif team_score == opponent_score:
@@ -35,9 +40,9 @@ def get_game_sequences_for_teams(games, teams, vector_dict):
 def split_and_shuffle_train_test(X, y, ratio):
     split = int(len(X) * ratio)
     print(len(X), split)
-    val = int(len(X[split:])/2)
+    val = int(len(X[split:]) / 2)
     print(len(X), split, val)
-    x_y = list(zip(X,y))
+    x_y = list(zip(X, y))
     random.seed(4)
     random.shuffle(x_y)
     shuffled_X = []
@@ -49,40 +54,46 @@ def split_and_shuffle_train_test(X, y, ratio):
     shuffled_y = np.array(shuffled_y)
     X_train = shuffled_X[:split]
     y_train = shuffled_y[:split]
-    X_test = shuffled_X[split:split+val]
-    y_test = shuffled_y[split:split+val]
-    X_val = shuffled_X[split+val:]
-    y_val = shuffled_y[split+val:]
+    X_test = shuffled_X[split : split + val]
+    y_test = shuffled_y[split : split + val]
+    X_val = shuffled_X[split + val :]
+    y_val = shuffled_y[split + val :]
     return X_train, y_train, X_test, y_test, X_val, y_val
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     games = session.query(Game).all()
-    print('got games')
-    vector_dict = load_vectors_to_dict('resources/normal_vectors_ids.txt')
+    print("got games")
     teams = session.query(Team).all()
-    print('got teams')
-    print('getting vectors...')
-    X, y = get_game_sequences_for_teams(games, teams, vector_dict)
-    print('got vectors')
-    X_train, y_train, X_test, y_test, X_val, y_val = split_and_shuffle_train_test(X, y, .8)
+    print("got teams")
+    print("getting vectors...")
+    X, y = get_game_sequences_for_teams(games, teams)
+    print("got vectors")
+    X_train, y_train, X_test, y_test, X_val, y_val = split_and_shuffle_train_test(
+        X, y, 0.8
+    )
     model = Sequential()
-    model.add(Dense(12, input_dim=42, activation='relu'))
-    model.add(Dense(8, activation='relu'))
-    model.add(Dense(3, activation='softmax'))
-    model.compile(loss='categorical_crossentropy',
-                  optimizer='adam', metrics=['accuracy'])
+    model.add(Dense(12, input_dim=42, activation="relu"))
+    model.add(Dense(8, activation="relu"))
+    model.add(Dense(3, activation="softmax"))
+    model.compile(
+        loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"]
+    )
     model.summary()
     filepath = "model-files/best_model.normalvecs.{epoch:02d}-{loss:.2f}.h5"
-    es = EarlyStopping(monitor='loss', patience=500, verbose=1, mode='min')
-    mc = ModelCheckpoint(filepath, save_best_only=True,
-                         monitor='loss', mode='min', verbose=1)
+    es = EarlyStopping(monitor="loss", patience=500, verbose=1, mode="min")
+    mc = ModelCheckpoint(
+        filepath, save_best_only=True, monitor="loss", mode="min", verbose=1
+    )
 
-    model.fit(X_train,
-              y_train,
-              epochs=3000,
-              batch_size=16,
-              validation_data=(X_test, y_test), callbacks=[mc, es])
-    #model = load_model('/home/daniel/git/hail_mary_rnn/model-files/best_model.1005-0.57.h5')
+    model.fit(
+        X_train,
+        y_train,
+        epochs=3000,
+        batch_size=16,
+        validation_data=(X_test, y_test),
+        callbacks=[mc, es],
+    )
+    # model = load_model('/home/daniel/git/hail_mary_rnn/model-files/best_model.1005-0.57.h5')
     scores = model.evaluate(X_val, y_val, verbose=1)
-    print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+    print("%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
--- hail_mary_rnn/prepare_team_vectors.py
+++ hail_mary_rnn/prepare_team_vectors.py
@@ -3,111 +3,167 @@ from models import *
 import numpy as np
 import re
 from sklearn import preprocessing
+from collections import OrderedDict
+
+MODEL_LIST = [
+    (Passing, 8),
+    (Receiving, 4),
+    (Rushing, 3),
+    (Secondary, 4),
+    (Tackles, 6),
+    (Fumbles, 4),
+    (KickReturns, 3),
+    (PuntReturns, 3),
+    (Kicks, 4),
+    (Punts, 2),
+]
+
 
 def load_vectors_to_dict(file):
     vector_dict = {}
-    with open(file, 'r') as f:
+    with open(file, "r") as f:
         for line in f:
-            player_id, vector = line.strip().split(' ')[0], line.strip().split(' ')[1:]
+            player_id, vector = line.strip().split(" ")[0], line.strip().split(" ")[1:]
             vector_dict[player_id] = np.array(vector, dtype=np.float32)
     return vector_dict
 
-def get_team_vectors(game_instance, vector_dict, injuries=None, prediction_team_id=None):
-    stat_models = [Passing, Rushing, Receiving, Secondary,
-                   Tackles, Fumbles, KickReturns, PuntReturns, Punts, Kicks]
-    home_players = []
-    away_players = []
 
-    away_player_vectors = []
-    home_player_vectors = []
-    
+def normalize_vector(career_stats):
+    vector = np.array(list(career_stats.values()))
+    avg_vector = [
+        abs(i) / career_stats["game_count"] if career_stats["game_count"] > 0 else 0
+        for i in career_stats.values()
+    ]
+    if career_stats["game_count"] > 36:
+        factor = 3
+        avg_vector = np.power(avg_vector, factor)
+    else:
+        factor = 1 + 2 * (career_stats["game_count"] / 36)
+        avg_vector = np.power(avg_vector, factor)
+    vector = vector * avg_vector
+
+    vector = np.array(vector)
+
+    return vector
+
+
+def get_career_stats_up_to_date(player, game_instance, model_list=MODEL_LIST):
+    career_stats = OrderedDict()
+    game_ids = []
+    for model_instance, features in model_list:
+        model_keys = [
+            model_instance.__tablename__ + "_" + k
+            for k in list(model_instance.get_stats(model_instance).keys())
+        ]
+        # total number of unique game_ids where this player_id appears
+        # Get stats filtered by date
+        all_stats = (
+            session.query(model_instance)
+            .filter_by(player_id=player)
+            .filter(Game.date < game_instance.date)  # Apply the date filter
+            .all()
+        )
+
+        if (len(all_stats)) == 0:
+            for i in range(0, features):
+                career_stats[model_keys[i]] = 0
+        else:
+            total_stats_dict = all_stats[0].get_stats()
+            # building the career stats dictionary
+            for stat in all_stats:
+                game_id = stat.game_id
+                if game_id not in game_ids:
+                    game_ids.append(game_id)
+                for key, value in stat.get_stats().items():
+                    total_stats_dict[key] += value
+            # normalizing some values in the career stats dictionary and adding to career_stats
+            for key, value in total_stats_dict.items():
+                if key == "yards":
+                    value = value / 100
+                if key == "qb_rating":
+                    value = value / 158.3
+                career_stats[model_instance.__tablename__ + "_" + key] = value
+    career_stats["game_count"] = len(game_ids)
+
+    return career_stats
+
+
+def get_team_vectors(team, game_instance):
+    stat_models = [
+        Passing,
+        Rushing,
+        Receiving,
+        Secondary,
+        Tackles,
+        Fumbles,
+        KickReturns,
+        PuntReturns,
+        Punts,
+        Kicks,
+    ]
+    players = []
+    player_vectors = []
+
     for stat_instance in stat_models:
-        for stat_model in session.query(stat_instance).filter_by(game_id=game_instance.id, team_id=game_instance.home_id).all():
-            if stat_model.player_id not in home_players:
-                home_players.append(stat_model.player_id)
-        for stat_model in session.query(stat_instance).filter_by(game_id=game_instance.id, team_id=game_instance.away_id).all():
-            if stat_model.player_id not in away_players:
-                away_players.append(stat_model.player_id)
-        for player in away_players:
-            away_player_vectors.append(vector_dict[str(player)])
+        for stat_model in (
+            session.query(stat_instance)
+            .filter_by(game_id=game_instance.id, team_id=team)
+            .all()
+        ):
+            if stat_model.player_id not in players:
+                players.append(stat_model.player_id)
 
-        for player in home_players:
-            home_player_vectors.append(vector_dict[str(player)])
+        for player in players:
+            # call function here
+            player_vectors.append(
+                normalize_vector(
+                    get_career_stats_up_to_date(str(player), game_instance)
+                )
+            )
 
-        if len(away_player_vectors) < 48:
-            for i in range(len(away_player_vectors), 48):
-                away_player_vectors.append(np.zeros(42))
-        
-        if len(home_player_vectors) < 48:
-            for i in range(len(home_player_vectors), 48):
-                home_player_vectors.append(np.zeros(42))
+        if len(player_vectors) < 48:
+            for i in range(len(player_vectors), 48):
+                player_vectors.append(np.zeros(42))
+    team_vector = preprocessing.scale(np.stack(player_vectors))
 
-    away_team_vector = np.stack(away_player_vectors)
-    home_team_vector = np.stack(home_player_vectors)
-    
-    return home_team_vector, away_team_vector
-    # if prediction_team_id:
-    #     if prediction_team_id == game_instance.home_id:
-    #         for player in home_players:
-    #             player_obj = session.query(Player).filter_by(id=player).first()
-    #             player_name = player_obj.first_name+' '+player_obj.last_name
-    #             if player_name in injuries['Player'].tolist():
-    #                 print('injured player: ', player_name)
-    #                 if injuries[injuries['Player']==player_name]['Class'].to_string()  != 'probable':
-    #                     pass
-    #             else:
-    #                 home_player_vectors.append(vector_dict[str(player)])
-    #         home_team_vector = np.stack(home_player_vectors)
-    #         if len(home_player_vectors) < 48:
-    #             for i in range(len(home_player_vectors), 48):
-    #                 home_player_vectors.append(np.zeros(42))
-    #         return home_team_vector, None
-    #     elif prediction_team_id == game_instance.away_id:
-    #         for player in away_players:
-    #             player_obj = session.query(Player).filter_by(id=player).first()
-    #             player_name = player_obj.first_name+' '+player_obj.last_name
-    #             if player_name in injuries['Player'].tolist():
-    #                 print('injured player: ', player_name)
-    #                 if injuries[injuries['Player']==player_name]['Class'].to_string() != 'probable':
-    #                     pass
-    #             else:
-    #                 away_player_vectors.append(vector_dict[str(player)])
-    #         away_team_vector = np.stack(away_player_vectors)
-
-    #         if len(away_player_vectors) < 48:
-    #             for i in range(len(away_player_vectors), 48):
-    #                 away_player_vectors.append(np.zeros(42))
-        
-
-    #         return None, away_team_vector     
+    return team_vector
 
 
-    # else:
+def get_home_away_vector(game_instance):
+    home_id = game_instance.home_id
+    away_id = game_instance.away_id
+
+    away_team_vector = get_team_vectors(away_id, game_instance)
+    home_team_vector = get_team_vectors(home_id, game_instance)
+
+    return away_team_vector, home_team_vector
 
 
-def get_game_vectors_and_scores(games, vector_dict):
+def get_game_vectors_and_scores(games):
     vectors_by_game = []
     for game in games:
-        away_team_vector, home_team_vector = get_team_vectors(game, vector_dict)
+        away_team_vector, home_team_vector = get_home_away_vector(game)
         vectors_by_game.append((away_team_vector, home_team_vector, game))
     return vectors_by_game
 
 
-def get_vector_by_team_and_game(games, Team, vector_dict):
+def get_vector_by_team_and_game(games, team_instance):
     vectors_and_scores_by_game_and_team = []
     for game in games:
         ids = game.home_id, game.away_id
-        if Team.id in ids:
-            home_team_vector, away_team_vector = get_team_vectors(game, vector_dict)
-            if Team.id == game.home_id:
+        if team_instance.id in ids:
+            away_team_vector, home_team_vector = get_home_away_vector(game)
+            if team_instance.id == game.home_id:
                 team_vector, opponent_vector = home_team_vector, away_team_vector
                 team_score = game.home_final
                 opponent_score = game.away_final
 
-            elif Team.id == game.away_id:
+            elif team_instance.id == game.away_id:
                 opponent_vector, team_vector = home_team_vector, away_team_vector
                 team_score = game.away_final
                 opponent_score = game.home_final
 
-            vectors_and_scores_by_game_and_team.append((team_vector, opponent_vector, team_score, opponent_score, game.date))
+            vectors_and_scores_by_game_and_team.append(
+                (team_vector, opponent_vector, team_score, opponent_score, game.date)
+            )
     return vectors_and_scores_by_game_and_team
